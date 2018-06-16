853892b8f35cd8336b8f914c23711d1baab30b23
BART96 <bart96@ro.ru>
2018-06-16 15:58:49 +0300

    .

--- test/test.js
+++ test/test.js
@@ -15,17 +15,17 @@ let minutes = now.getUTCMinutes();  minutes = minutes > 9 ? minutes : '0'+ minut
 describe('Тестирование без дополнительных параметров', () => {
   let format = new Format({UTC:true});
 
-  it('#1', () => assert.equal(format.date(), `${year}.${month}.${day} ${hours}:${minutes}`));
-  it('#2', () => assert.equal(format.date(0), '1970.01.01 00:00'));
-  it('#3', () => assert.equal(format.date(1000000000000), '2001.09.09 01:46'));
-  it('#4', () => assert.equal(format.date(1000000000000, 'Year: yyyy. Day: dd'), 'Year: 2001. Day: 09'));
+  it('#1 - format.date()', () => assert.equal(format.date(), `${year}.${month}.${day} ${hours}:${minutes}`));
+  it('#2 - format.date(0)', () => assert.equal(format.date(0), '1970.01.01 00:00'));
+  it('#3 - format.date(1000000000000)', () => assert.equal(format.date(1000000000000), '2001.09.09 01:46'));
+  it('#4 - format.date(1000000000000, \'Year: yyyy. Day: dd\')', () => assert.equal(format.date(1000000000000, 'Year: yyyy. Day: dd'), 'Year: 2001. Day: 09'));
 });
 
 describe('Тестирование с дополнительными параметрами', () => {
   let format = new Format({UTC:true, form:'[HH:MM]'});
 
-  it('#1', () => assert.equal(format.date(), `[${hours}:${minutes}]`));
-  it('#2', () => assert.equal(format.date(0), '[00:00]'));
-  it('#3', () => assert.equal(format.date(1000000000000), '[01:46]'));
-  it('#4', () => assert.equal(format.date(1000000000000, 'Year: yyyy. Hours: HH'), 'Year: 2001. Hours: 01'));
+  it('#1 - format.date()', () => assert.equal(format.date(), `[${hours}:${minutes}]`));
+  it('#2 - format.date(0)', () => assert.equal(format.date(0), '[00:00]'));
+  it('#3 - format.date(1000000000000)', () => assert.equal(format.date(1000000000000), '[01:46]'));
+  it('#4 - format.date(1000000000000, \'Year: yyyy. Hours: HH\')', () => assert.equal(format.date(1000000000000, 'Year: yyyy. Hours: HH'), 'Year: 2001. Hours: 01'));
 });
